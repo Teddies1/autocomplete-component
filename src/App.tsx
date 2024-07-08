3726ee2019bceec1419ccbecf24da2f64479a1f0
Teddies1 <nghonjoo@gmail.com>
2024-07-08 23:40:42 +0800

    installed tailwind and did test if works

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Jenny moment</h2>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
