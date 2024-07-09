757322572c23ec9122f952d24bc7628f5fa7d4c0
Teddies1 <nghonjoo@gmail.com>
2024-07-09 19:15:11 +0800

    install packages and added search options to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { useState } from 'react';
 import AutoComplete from './searchbar/AutoComplete';
+import SearchResults from './searchbar/SearchResults';
 
 function App() {
+  const [results, setResults] = useState([]);
   return (
     <>
-      <AutoComplete />
+      <div className='w-full h-[100vh] search-container flex flex-col justify-center items-center'>
+        <AutoComplete setResults={setResults} />
+        <SearchResults results={results} />
+      </div>
     </>
   )
 }
