12ef4eebf8c03b17cd6839155f430adbe0100c6d
Teddies1 <nghonjoo@gmail.com>
2024-07-09 21:51:48 +0800

    finished synchronous autocomplete

--- src/searchbar/AutoComplete.tsx
+++ src/searchbar/AutoComplete.tsx
@@ -4,74 +4,90 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { debounce } from '@mui/material/utils';
-
-type AutoCompleteProps = {
-    // description: string,
-
-}
+import CircularProgress from '@mui/material/CircularProgress';
 
 const AutoComplete = ({ setResults }: any) => {
     const [input, setInput] = useState("");
     const [options, setOptions] = useState<Array<Object>>([]);
+    const [disabled, setDisabled] = useState(false);
+    const loading = !disabled && options.length === 0;
+    const description = "test";
+    const label = "label";
 
-    // const fetchData = (value: string) => {
-    //   fetch("https://api.first.org/data/v1/countries")
-    //       .then((response) => response.json())
-    //       .then((json) => {
-    //           const countrydata: Array<Object> = Object.values(json.data); 
-    //           const options = countrydata.filter((countries) => {
-    //               return (
-    //                   value && 
-    //                   countries && 
-    //                   (countries as any).country && 
-    //                   (countries as any).country.toLowerCase().includes(value)
-    //               );
-    //           });
-    //           console.log(options);
-    //       });
-    // };
+    function sleep(duration: number): Promise<void> {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, duration);
+      });
+    }
+    
     React.useEffect(() => {
-      fetch("https://api.first.org/data/v1/countries")
-          .then((response) => response.json())
-          .then((json) => {
-              const countrydata: Array<Object> = Object.values(json.data); 
-              console.log(countrydata);
-              setOptions(countrydata);
-              console.log(options);
+      let active = true;
+  
+      if (!loading) {
+        return undefined;
+      }
+  
+      (async () => {
+        await sleep(1e3); // For demo purposes.
+        if (active) {
+          fetch("https://api.first.org/data/v1/countries")
+            .then((response) => response.json())
+            .then((json) => {
+                const countrydata: Array<Object> = Object.values(json.data); 
+                console.log(countrydata);
+                setOptions(countrydata);
+                console.log(options);
           });
-    }, []);
+        }
+      })();
+  
+      return () => {
+        active = false;
+      };
+    }, [loading]);
 
-    const onInputChange = (value: string) => {
-        setInput(value);
-        fetchData(value);
+    const onChangeHandle = (value) => {
+      // use the changed value to make request and then use the result. Which
+      console.log(value);
+      fetch("https://api.first.org/data/v1/countries")
+        .then((response) => response.json())
+        .then((json) => {
+            const countrydata: Array<Object> = Object.values(json.data); 
+            console.log(countrydata);
+            setOptions(countrydata);
+            console.log(options);
+        });
     };
-    // const options = ["jenny"]
+
     return (
       <>
         <div className="">
           <div className="input-container bg-slate-300 pl-4 pr-6 py-2 shadow-lg flex flex-col justify-center items-center gap-5 rounded-md">  
-            {/* <FaSearch/>    
-            <input 
-                placeholder='Type to begin searching' className='pl-2 pr-6 py-2 w-full h-full border border-slate-500 bg-transparent border-none focus:outline-none' 
-                value={input}
-                onChange={(e) => onInputChange(e.target.value)}
-            >
-            </input> */}
+            <div>{label}</div>
             <Autocomplete
-              multiple
-              disablePortal
-              id="combo-box-demo"
+              id="autocomplete-demo"
               options={options}
-              filterOptions={(x) => x}
+              disabled={disabled}
+              loading = {loading}
+              getOptionLabel={(options) => `${options.country}, ${options.region}`}
+              isOptionEqualToValue={(option, value) => option.country === value.country}
               sx={{ width: 300 }}
+              
               renderInput={(params) => 
                 <TextField 
                   {...params} 
-                  label="Type to begin searching" 
-                  onChange={(e) => onInputChange(e.target.value)}
+                  label="Type to begin searching"
+                  onChange={e => {
+                    if (e.target.value !== "" || e.target.value !== null) {
+                      onChangeHandle(e.target.value);
+                    }
+                  }} 
                 />
               }
             />
+            <div>{description}</div>
           </div>
         </div>
         
