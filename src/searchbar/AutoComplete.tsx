10c046a3e27479b54c12ecfefa8240fbcf41fa6d
Teddies1 <nghonjoo@gmail.com>
2024-07-09 19:14:56 +0800

    added tsx file for search options

--- src/searchbar/AutoComplete.tsx
+++ src/searchbar/AutoComplete.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 type AutoCompleteProps = {
@@ -6,47 +6,46 @@ type AutoCompleteProps = {
 
 }
 
-const AutoComplete = (props: AutoCompleteProps) => {
-  const [input, setInput] = useState("");
-  const fetchData = (value) => {
-    fetch("https://api.first.org/data/v1/countries")
-      .then((response) => response.json())
-      .then((json) => {
-        const countrydata: Array<Object> = Object.values(json.data);
-        const options = countrydata.filter((countries) => {
-            return (
-                value && 
-                countries && 
-                (countries as any).country && 
-                (countries as any).country.toLowerCase().includes(value)
-            );
-        });
-        console.log(options);
-      });
-  };
+const AutoComplete = ({ setResults }: any) => {
+    const [input, setInput] = useState("");
 
-  const onInputChange = (value) => {
-    setInput(value);
-    fetchData(value);
-  };
+    const fetchData = (value: string) => {
+      fetch("https://api.first.org/data/v1/countries")
+          .then((response) => response.json())
+          .then((json) => {
+              const countrydata: Array<Object> = Object.values(json.data);
+              const options = countrydata.filter((countries) => {
+                  return (
+                      value && 
+                      countries && 
+                      (countries as any).country && 
+                      (countries as any).country.toLowerCase().includes(value)
+                  );
+              });
+              setResults(options);
+          });
+    };
 
-  return (
-    <>
-      
-      <div className="w-full h-[100vh] flex gap-10 flex-col justify-center items-center">
-        <div className="input-container bg-slate-300 pl-4 pr-6 py-2 shadow-lg flex justify-center items-center gap-5 rounded-md">  
-          <FaSearch/>    
-          <input 
-              placeholder='Type to begin searching' className='pl-2 pr-6 py-2 w-full border border-slate-500 bg-transparent border-none focus:outline-none' 
-              value={input}
-              onChange={(e) => onInputChange(e.target.value)}
-          >
-          </input>
+    const onInputChange = (value: string) => {
+        setInput(value);
+        fetchData(value);
+    };
+
+    return (
+      <>
+        <div className="">
+          <div className="input-container bg-slate-300 pl-4 pr-6 py-2 shadow-lg flex justify-center items-center gap-5 rounded-md">  
+            <FaSearch/>    
+            <input 
+                placeholder='Type to begin searching' className='pl-2 pr-6 py-2 w-full border border-slate-500 bg-transparent border-none focus:outline-none' 
+                value={input}
+                onChange={(e) => onInputChange(e.target.value)}
+            >
+            </input>
+          </div>
         </div>
-      </div>
-      
-    </>
-  )    
-}
+      </>
+    )    
+  }
 
 export default AutoComplete
---
+++ src/searchbar/SearchResults.tsx
@@ -0,0 +1,21 @@
+import {
+    useFloating,
+    autoUpdate,
+    offset,
+    flip,
+    shift,
+    FloatingPortal,
+  } from '@floating-ui/react';
+import React from 'react'
+
+const SearchResults = () => {
+  return (
+    <>
+        <div className='options-list'>
+            <div className='text-pink-300'>test</div>
+        </div>
+    </>
+  );
+}
+
+export default SearchResults
