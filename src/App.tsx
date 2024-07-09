3cec960c6958f8775e986a134c26cb6ffab05ca6
Teddies1 <nghonjoo@gmail.com>
2024-07-10 00:27:28 +0800

    fixed formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import './App.css'
-import { useState } from 'react';
 import AutoComplete from './searchbar/AutoComplete';
-import SearchResults from './searchbar/SearchResults';
 
 function App() {
-  const [results, setResults] = useState([]);
   return (
     <>
-      <div className='bg-white w-2/4 m-auto h-[100vh] gap-2 search-container flex flex-col justify-center items-center'>
-        <AutoComplete  description="With default display and search on focus" label="Sync Search" loading={false} />
-        <AutoComplete  description="With description and custom results display" label="Async Search" loading={true} />
+      <div className='search-container bg-white w-2/4 m-auto h-[100vh] gap-2 flex flex-col justify-center items-center'>
+        <AutoComplete  
+          description="With default display and search on focus" 
+          label="Sync Search" 
+          loading={false} 
+          multiple={true}
+        />
+        <AutoComplete  
+          description="With description and custom results display" 
+          label="Async Search" 
+          loading={true} 
+          multiple={false}
+        />
+        <AutoComplete  
+          description="With description and custom results display" 
+          label="Async Search with Multi-select" 
+          loading={true} 
+          multiple={true}
+        />
       </div>
     </>
   )
