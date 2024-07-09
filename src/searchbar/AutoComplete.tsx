1af38a11ce0c1ca9055da988dcc628076f0288cd
Teddies1 <nghonjoo@gmail.com>
2024-07-09 23:50:31 +0800

    finished logic for async and sync implementations based on input props

--- src/searchbar/AutoComplete.tsx
+++ src/searchbar/AutoComplete.tsx
@@ -1,65 +1,38 @@
 import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { debounce } from '@mui/material/utils';
 import CircularProgress from '@mui/material/CircularProgress';
 
-const AutoComplete = ({ setResults }: any) => {
-    const [input, setInput] = useState("");
+const AutoComplete = ({ description, label, loading }) => {
     const [options, setOptions] = useState<Array<Object>>([]);
     const [disabled, setDisabled] = useState(false);
-    const loading = !disabled && options.length === 0;
-    const description = "With default display and search on focus";
-    const label = "Sync Search";
-
-    function sleep(duration: number): Promise<void> {
+    
+    
+    function sleep(duration: number = 0): Promise<void> {
       return new Promise<void>((resolve) => {
         setTimeout(() => {
           resolve();
         }, duration);
       });
     }
-
-    React.useEffect(() => {
-      let active = true;
-  
-      if (!loading) {
-        return undefined;
-      }
-  
-      (async () => {
-        await sleep(1e3); // For demo purposes.
-        if (active) {
-          fetch("https://api.first.org/data/v1/countries")
-            .then((response) => response.json())
-            .then((json) => {
-                const countrydata: Array<Object> = Object.values(json.data); 
-                console.log(countrydata);
-                setOptions(countrydata);
-                console.log(options);
-          });
-        }
-      })();
-  
-      return () => {
-        active = false;
-      };
-    }, [loading]);
-
-    const onChangeHandle = (value) => {
-      // use the changed value to make request and then use the result. Which
-      console.log(value);
+    
+    const onInputChange = (value: string) => {
       fetch("https://api.first.org/data/v1/countries")
         .then((response) => response.json())
         .then((json) => {
-            const countrydata: Array<Object> = Object.values(json.data); 
-            console.log(countrydata);
-            setOptions(countrydata);
-            console.log(options);
+            const countrydata: Array<Object> = Object.values(json.data);
+            const filtered: any = countrydata.filter((countries) => {
+              return countries && 
+              (countries as any).country && 
+              (countries as any).country.toLowerCase().includes(value)
+            })
+            setOptions(filtered);
         });
     };
+    
+    const delayedCall = debounce(onInputChange, 1000);
 
     return (
       <>
@@ -67,21 +40,28 @@ const AutoComplete = ({ setResults }: any) => {
           <div className="input-container bg-slate-300 pl-4 pr-6 py-2 shadow-lg flex flex-col justify-center items-center gap-5 rounded-md">  
             <div>{label}</div>
             <Autocomplete
+              multiple
               id="autocomplete-demo"
               options={options}
               disabled={disabled}
               loading = {loading}
+              filterOptions={(options) => options}
               getOptionLabel={(options) => `${options.country}, ${options.region}`}
               isOptionEqualToValue={(option, value) => option.country === value.country}
               sx={{ width: 300 }}
-              
               renderInput={(params) => 
                 <TextField 
                   {...params} 
                   label="Type to begin searching"
-                  onChange={e => {
-                    if (e.target.value !== "" || e.target.value !== null) {
-                      onChangeHandle(e.target.value);
+                  onChange={(e) => {
+                    console.log(loading);
+                    if (loading === true && (e.target.value !== "" || e.target.value !== null)) {
+                      console.log("async");
+                      delayedCall(e.target.value);
+                    }
+                    else if(loading === false && (e.target.value !== "" || e.target.value !== null)){
+                      console.log("sync");
+                      onInputChange(e.target.value);
                     }
                   }} 
                 />
