582353f81ed970de9c1c9f46a28f7d4f59c48d1e
Teddies1 <nghonjoo@gmail.com>
2024-07-09 16:33:32 +0800

    styled search bar to have search icons

--- src/searchbar/AutoComplete.tsx
+++ src/searchbar/AutoComplete.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 type AutoCompleteProps = {
     // description: string,
@@ -6,13 +7,17 @@ type AutoCompleteProps = {
 }
 
 const AutoComplete = (props: AutoCompleteProps) => {
+  
   return (
     <>
-      <div className="w-full h-[100vh] flex justify-center items-center">
-        <div className="">
-          <input placeholder='Type to begin searching' className='px-6 py-2 w-full border border-slate-500'></input>
+      
+      <div className="w-full h-[100vh] flex gap-10 flex-col justify-center items-center">
+        <div className="input-container bg-slate-300 pl-4 pr-6 py-2 shadow-lg flex justify-center items-center gap-5 rounded-md">  
+          <FaSearch/>    
+          <input placeholder='Type to begin searching' className='pl-2 pr-6 py-2 w-full border border-slate-500 bg-transparent border-none focus:outline-none'></input>
         </div>
       </div>
+      
     </>
   )    
 }
